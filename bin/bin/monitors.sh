8a955251b2c96e43bc606e5224f7b84ace2a55c7
Sri Ramanujam <sri@ramanujam.io>
2021-09-25 17:21:09 -0400

    Deleting all the previous config.
    At this point I can't find anything I'm missing and the Ansible approach
    has proven itself out. Time to delete all the old cruft.
    
    rip all this history (except it's in the git repo forever)

--- bin/bin/monitors.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(xrandr -q | grep "HDMI1 connected") ]]
-then
-    $(xrandr --output HDMI1 --auto --output eDP1 --off)
-else
-    $(xrandr --output eDP1 --auto --output HDMI1 --off)
-fi
